b348cf3312518cda1c0784469fc84f21ab64349f
Amandeep <ads71993@gmail.com>
2019-02-19 22:26:16 -0500

    added test cases for SocketNB

---
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/client/SocketNBTest.java
@@ -0,0 +1,69 @@
+package edu.northeastern.ccs.im.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SocketNBTest {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 4545;
+    private static SocketNB socketNBObj;
+    static ServerSocketChannel serverSocket;
+
+    @BeforeAll
+    static void suiteSetUp() throws IOException {
+        serverSocket = SelectorProvider.provider().openServerSocketChannel();
+        serverSocket.configureBlocking(false);
+        serverSocket.socket().bind(new InetSocketAddress(PORT));
+    }
+
+    @BeforeEach
+    public void setUp() {
+        socketNBObj = new SocketNB(HOST, PORT);
+    }
+
+    @Test
+    public void testSocketConnection() throws IOException {
+        //        check the socket status without any channel
+        assertEquals(false, socketNBObj.isConnected());
+        socketNBObj.startIMConnection();
+        assertEquals(true, socketNBObj.isConnected());
+    }
+
+    @Test
+    public void testPrintMessage() throws IOException {
+        Message message = Message.makeBroadcastMessage(null,"hello world");
+        try {
+            socketNBObj.print(message);
+        }catch (IllegalOperationException e){
+            assertEquals("Cannot send a message when we are not connected!",
+                    e.getMessage());
+        }
+        socketNBObj.startIMConnection();
+        socketNBObj.print(message);
+    }
+
+    @Test
+    public void testEnqueueMessages() throws IOException {
+        List<Message> messageList  = new ArrayList<>();
+        Message message = Message.makeBroadcastMessage(null,"hello world");
+        messageList.add(message);
+        messageList.add(message);
+        messageList.add(message);
+        socketNBObj.startIMConnection();
+        socketNBObj.startIMConnection();
+
+        socketNBObj.enqueueMessages(messageList);
+    }
+
+}
---
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/PrattleTest.java
@@ -0,0 +1,91 @@
+package edu.northeastern.ccs.im.server;
+
+import edu.northeastern.ccs.im.NetworkConnection;
+import edu.northeastern.ccs.im.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class PrattleTest {
+    private ServerSocketChannel serverSocket;
+    private Selector selector;
+    private ScheduledExecutorService tPool;
+    private SocketChannel sc;
+
+    @BeforeEach
+    void setUp() {
+
+        try {
+            sc = SocketChannel.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testMain() throws IOException {
+//        serverSocket = ServerSocketChannel.open();
+//        serverSocket.configureBlocking(false);
+//        serverSocket.socket().bind(new InetSocketAddress(4545));
+////        Prattle.main(new String[] {"4545"});
+//        sc.connect(serverSocket.socket().getLocalSocketAddress());
+//
+//        ByteBuffer wrapper = ByteBuffer.wrap("test".getBytes());
+//        int bytesWritten = 0;
+//        bytesWritten += sc.write(wrapper);
+    }
+
+    @Test
+    void testStopServer() {
+        Prattle.stopServer();
+    }
+
+    @Test
+    void testBroadcastClient() throws IllegalAccessException,
+            NoSuchFieldException{
+        ConcurrentLinkedQueue<ClientRunnable> queue = new ConcurrentLinkedQueue<>();
+        NetworkConnection nc = new NetworkConnection(sc);
+
+        ClientRunnable c1 = new ClientRunnable(nc);
+        queue.add(c1);
+
+        Field active = Prattle.class.getDeclaredField("active");
+        active.setAccessible(true);
+        active.set(null, queue);
+
+        ConcurrentLinkedQueue<ClientRunnable> returned = (ConcurrentLinkedQueue<ClientRunnable>) active.get(null);
+
+        Prattle.broadcastMessage(Message.makeBroadcastMessage("test","hello"));
+
+    }
+
+    @Test
+    void testRemoveClient() throws IllegalAccessException, NoSuchFieldException {
+        ConcurrentLinkedQueue<ClientRunnable> queue = new ConcurrentLinkedQueue<>();
+
+        NetworkConnection nc = new NetworkConnection(sc);
+        ClientRunnable c1 = new ClientRunnable(nc);
+        queue.add(c1);
+        Field active = Prattle.class.getDeclaredField("active");
+        active.setAccessible(true);
+        active.set(null, queue);
+        ConcurrentLinkedQueue<ClientRunnable> returned = (ConcurrentLinkedQueue<ClientRunnable>) active.get(null);
+
+        Prattle.removeClient(c1);
+
+        assertTrue(returned.isEmpty());
+    }
+
+}
