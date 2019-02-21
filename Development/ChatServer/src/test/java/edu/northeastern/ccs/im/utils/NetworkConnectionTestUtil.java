5bd721038262b2593b5ae1e6b5b206f82e1835e0
varsharaom <rao.va@husky.neu.edu>
2019-02-20 22:35:26 -0500

    address PR request comments

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/utils/NetworkConnectionTestUtil.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/utils/NetworkConnectionTestUtil.java
@@ -7,205 +7,190 @@ import java.util.*;
 
 public class NetworkConnectionTestUtil {
 
-    private NetworkConnectionTestUtil() {
+	private NetworkConnectionTestUtil() {
 
-    }
+	}
 
-    public static Iterator<Message> getMessageIterator() {
+	public static Iterator<Message> getMessageIterator() {
 
-        return new Iterator<Message>() {
-            Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            Message message2 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
-            int index = 0;
+		return new Iterator<Message>() {
+			Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message2 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
+			int index = 0;
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
 
-            @Override
-            public Message next() {
-                if(hasNext()) {
-                    return messageList.get(index++);
-                }
-                else {
-                    throw new NoSuchElementException();
-                }
-            }
-        };
-    }
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
+			}
+		};
+	}
 
-    public static Iterator<Message> getMessageIteratorWithNoUsers() {
-        return new Iterator<Message>() {
-            Message message1 = Message.makeBroadcastMessage(null,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message1));
-            int index = 0;
+	public static Iterator<Message> getMessageIteratorWithNoUsers() {
+		return new Iterator<Message>() {
+			Message message1 = Message.makeBroadcastMessage(null, MessageConstants.BROADCAST_TEXT_MESSAGE);
+			List<Message> messageList = new ArrayList<>(Arrays.asList(message1));
+			int index = 0;
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
 
-            @Override
-            public Message next() {
-                if(hasNext()) {
-                    return messageList.get(index++);
-                }
-                else {
-                    throw new NoSuchElementException();
-                }
-            }
-        };
-    }
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
+			}
+		};
+	}
 
-    public static Iterator<Message> getMessageIteratorWithDifferentUsers() {
+	public static Iterator<Message> getMessageIteratorWithDifferentUsers() {
 
-        return new Iterator<Message>() {
-            Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            Message message2 = Message.makeBroadcastMessage(MessageConstants.SECOND_USER,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
-            int index = 0;
+		return new Iterator<Message>() {
+			Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message2 = Message.makeBroadcastMessage(MessageConstants.SECOND_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
+			int index = 0;
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
 
-            @Override
-            public Message next() {
-                if(hasNext()) {
-                    return messageList.get(index++);
-                }
-                else {
-                    throw new NoSuchElementException();
-                }
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
 
-            }
-        };
-    }
+			}
+		};
+	}
 
-    public static Iterator<Message> getMessageIteratorWithQuitMessages() {
+	public static Iterator<Message> getMessageIteratorWithQuitMessages() {
 
-        return new Iterator<Message>() {
-            Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            Message message2 = Message.makeQuitMessage(MessageConstants.SECOND_USER);
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
-            int index = 0;
+		return new Iterator<Message>() {
+			Message message1 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message2 = Message.makeQuitMessage(MessageConstants.SECOND_USER);
+			List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2));
+			int index = 0;
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
 
-            @Override
-            public Message next() {
-                if(hasNext()) {
-                    return messageList.get(index++);
-                }
-                else {
-                    throw new NoSuchElementException();
-                }
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
 
-            }
-        };
-    }
+			}
+		};
+	}
 
+	public static Iterator<Message> getMessageIteratorWithManyMessages() {
+		return new Iterator<Message>() {
+			Message message5 = Message.makeSimpleLoginMessage(MessageConstants.FIRST_USER);
+			Message message4 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message3 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
+					MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message2 = Message.makeBroadcastMessage(null, MessageConstants.BROADCAST_TEXT_MESSAGE);
+			Message message1 = Message.makeQuitMessage(MessageConstants.SIMPLE_USER);
+			Message message0 = Message.makeSimpleLoginMessage(MessageConstants.FIRST_USER);
 
-    public static Iterator<Message> getMessageIterator1() {
-        return new Iterator<Message>() {
-        	Message message2 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                     MessageConstants.BROADCAST_TEXT_MESSAGE);
-            Message message1 = Message.makeQuitMessage(
-                    MessageConstants.SIMPLE_USER);
-            
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message2,message1));
-            int index = 0;
+			List<Message> messageList = new ArrayList<>(
+					Arrays.asList(message5, message4, message3, message2, message1, message0));
+			int index = 0;
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
 
-            @Override
-            public Message next() {
-                return messageList.get(index++);
-            }
-        };
-    }
-    
-    public static Iterator<Message> getMessageIterator2() {
-        return new Iterator<Message>() {
-        	Message message3 = Message.makeBroadcastMessage(MessageConstants.SIMPLE_USER,
-                     MessageConstants.BROADCAST_TEXT_MESSAGE);
-        	Message message2 = Message.makeBroadcastMessage(null,
-                    MessageConstants.BROADCAST_TEXT_MESSAGE);
-            Message message1 = Message.makeQuitMessage(
-                    MessageConstants.SIMPLE_USER);
-            
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message3,message2,message1));
-            int index = 0;
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			}
+		};
+	}
 
-            @Override
-            public Message next() {
-                return messageList.get(index++);
-            }
-        };
-    }
-    
-    public static Iterator<Message> getMessageIterator3() {
-        return new Iterator<Message>() {
-        	
-            Message message1 = Message.makeQuitMessage(
-                    MessageConstants.SIMPLE_USER);
-            
-            Message message2 = Message.makeQuitMessage(
-                    MessageConstants.SIMPLE_USER);
-            List<Message> messageList = new ArrayList<>(Arrays.asList(message1,message2));
-            int index = 0;
+	public static Iterator<Message> getMessageIteratorWithMultipleQuitMessages() {
+		return new Iterator<Message>() {
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			Message message1 = Message.makeQuitMessage(MessageConstants.SIMPLE_USER);
 
-            @Override
-            public Message next() {
-                return messageList.get(index++);
-            }
-        };
-    }
-    
-    
-    public static Iterator<Message> getMessageIterator4() {
-        return new Iterator<Message>() {
-        	
-           
-            List<Message> messageList = new ArrayList<>();
-            int index = 0;
+			Message message2 = Message.makeQuitMessage(MessageConstants.SIMPLE_USER);
 
-            @Override
-            public boolean hasNext() {
-                return index < messageList.size();
-            }
+			Message message3 = Message.makeQuitMessage(MessageConstants.SIMPLE_USER);
+			List<Message> messageList = new ArrayList<>(Arrays.asList(message1, message2, message3));
+			int index = 0;
 
-            @Override
-            public Message next() {
-                return messageList.get(index++);
-            }
-        };
-    }
-    
-    
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
+
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
+
+			}
+		};
+	}
+
+	public static Iterator<Message> getMessageIteratorWithNoMessages() {
+		return new Iterator<Message>() {
+
+			List<Message> messageList = new ArrayList<>();
+			int index = 0;
+
+			@Override
+			public boolean hasNext() {
+				return index < messageList.size();
+			}
+
+			@Override
+			public Message next() {
+				if (hasNext()) {
+					return messageList.get(index++);
+				} else {
+					throw new NoSuchElementException();
+				}
+			}
+		};
+	}
 }
