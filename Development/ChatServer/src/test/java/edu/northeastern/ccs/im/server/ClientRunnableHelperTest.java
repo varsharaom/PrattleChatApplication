3bd291dcff1b17d91bb18e18307d8e302a4ea98d
esethuraman <elattp@gmail.com>
2019-03-13 22:59:35 -0400

    file renamed

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/ClientRunnableHelperTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/ClientRunnableHelperTest.java
@@ -28,7 +28,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ClientRunnableHelperTests {
+public class ClientRunnableHelperTest {
 
     @InjectMocks
     ClientRunnable clientRunnable;
